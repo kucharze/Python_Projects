4a4452c2b11f2cfce1e0b9114bfbbdc9935da52f
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2025-05-14 16:40:59 -0400

    New text route

--- choose_your_own_adv.py
+++ choose_your_own_adv.py
@@ -21,6 +21,27 @@ if answer == "left":
 
 
 elif answer == "right":
+    answer = input(
+        "You come to a bridge, it looks wobbly, do you want to cross it or head back (cross/back)? ").lower()
+
+    if answer == "back":
+        print("You go back and lose.")
+
+    elif answer == "cross":
+        answer = input(
+            "You cross the bridge and meet a stranger. Do you talk to them (yes/no)? ").lower()
+
+        if answer == "yes":
+            print("You talk to the stranger and they give you gold. You win!")
+        
+        elif answer == "no":
+            print("You ignore the stranger and they are offended. They chop off your head and you lose.")
+        
+        else:
+            print("Not a valid option. You lose!")
+    
+    else:
+        print("Not a valid option. You lose!")
 
 
 else:
