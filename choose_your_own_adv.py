a7c41f8f1b905a7a5df2645860f3caa466dce715
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2025-05-14 16:34:43 -0400

    Added initial options

--- choose_your_own_adv.py
+++ choose_your_own_adv.py
@@ -5,3 +5,23 @@ print("Welcome", name, "to this adventure!")
 
 answer = input(
     "You are on a dirt road, it has come to an end and you can go left or right. Which way would you like to go? ").lower()
+
+if answer == "left":
+    answer = input(
+        "You come to a river, you can walk around it or swim across? Type walk to walk around and swim to swim across: ").lower()
+    
+    if answer == "swim":
+        print("You swam across and were eaten by an alligator.")
+    
+    elif answer == "walk":
+        print("You walked for many miles, ran out of water and you lost the game.")
+    
+    else:
+        print("Not a valid option. You lose!")
+
+
+elif answer == "right":
+
+
+else:
+    print("Not a valid option. You lose!")
