c858ff31667a87faa87d567b5d6c39e7036563ba
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2025-05-14 16:31:37 -0400

    New file for choose your own adventure game

---
+++ choose_your_own_adv.py
@@ -0,0 +1,7 @@
+#Based off of a coding project in the following video: https://www.youtube.com/watch?v=NpmFbWO6HPU
+
+name = input("What is your name? ")
+print("Welcome", name, "to this adventure!")
+
+answer = input(
+    "You are on a dirt road, it has come to an end and you can go left or right. Which way would you like to go? ").lower()
