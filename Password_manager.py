b727adedc03b456be1a2f9c1636fa08d08c2d2ad
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2025-05-15 20:23:23 -0400

    Comment for reference

--- Password_manager.py
+++ Password_manager.py
@@ -1,3 +1,5 @@
+#Based off of a coding project in the following video: https://www.youtube.com/watch?v=NpmFbWO6HPU
+
 pwd = input("What is the master password? ")
 with open("passwords.txt", 'r') as f:
     for line in f.readlines():
