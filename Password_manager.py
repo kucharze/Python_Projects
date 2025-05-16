b3d4eb4c44daf1eb6561f758e3b679102dfa39b8
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2025-05-15 20:25:29 -0400

    Options

--- Password_manager.py
+++ Password_manager.py
@@ -3,8 +3,22 @@
 pwd = input("What is the master password? ")
 
 mode = input(
-    "Would you like to add an entry, view entries, or quit? (add/view/quit) ")
+    "Would you like to add an entry, view entries, or quit? (add/view/quit) ").lower()
 
+if mode == "quit":
+    quit()
+
+elif mode == "view":
+    with open("passwords.txt", 'r') as f:
+        for line in f.readlines():
+            print(line)
+
+elif mode == "add":
+    service = input("What service is the password for? ")
+    password = input("What is the password? ")
+
+    with open("passwords.txt", 'a') as f:
+        f.write(service + ":" + password + "\n")
 
 
 with open("passwords.txt", 'r') as f:
