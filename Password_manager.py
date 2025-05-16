da5d686a4679cfc0819887b587ee58619f46abdc
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2025-05-15 20:24:30 -0400

    Selection for mode

--- Password_manager.py
+++ Password_manager.py
@@ -1,6 +1,12 @@
 #Based off of a coding project in the following video: https://www.youtube.com/watch?v=NpmFbWO6HPU
 
 pwd = input("What is the master password? ")
+
+mode = input(
+    "Would you like to add an entry, view entries, or quit? (add/view/quit) ")
+
+
+
 with open("passwords.txt", 'r') as f:
     for line in f.readlines():
         data = line.rstrip()
