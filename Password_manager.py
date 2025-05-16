388c0311549719acc8a70f8255fcc804954a245c
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2025-05-15 21:10:05 -0400

    Moved functions

--- Password_manager.py
+++ Password_manager.py
@@ -1,13 +1,12 @@
 #Based off of a coding project in the following video: https://www.youtube.com/watch?v=NpmFbWO6HPU
 
-pwd = input("What is the master password? ")
 
 def view():
     with open("passwords.txt", 'r') as f:
         for line in f.readlines():
             data = line.rstrip()#Rstrip is to remove the \n
             user, passw = data.split("|")#split the data into array
-            print("User: ", user, " Password: ", passw)
+            print("User: ", user, ", Password: ", passw)
             
 
 def add():
@@ -16,6 +15,10 @@ def add():
 
     with open("passwords.txt", 'a') as f:#Open file for this section only for appending
         f.write(name + "|" + pwd + "\n")
+
+
+pwd = input("What is the master password? ")
+
 
 mode = input(
     "Would you like to add an entry, view entries, or quit? (add/view/quit) ").lower()
