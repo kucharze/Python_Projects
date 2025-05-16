8c7bf6a2f262c5febd103d4f2799c7c2dd153665
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2025-05-15 20:38:59 -0400

    Move code under functions

--- Password_manager.py
+++ Password_manager.py
@@ -3,10 +3,14 @@
 pwd = input("What is the master password? ")
 
 def view():
-    pass
+    with open("passwords.txt", 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            service, password = data.split(":")
+            print("Service: " + service + " | Password: " + password)
 
 def add():
-    name = inpiut("Account Name: ")
+    name = input("Account Name: ")
     pwd = input("Password: ")
 
     with open("passwords.txt", 'a') as f:#Open file for this section only for appending
@@ -24,15 +28,4 @@ elif mode == "view":
             print(line)
 
 elif mode == "add":
-    service = input("What service is the password for? ")
-    password = input("What is the password? ")
-
-    with open("passwords.txt", 'a') as f:
-        f.write(service + ":" + password + "\n")
-
-
-with open("passwords.txt", 'r') as f:
-    for line in f.readlines():
-        data = line.rstrip()
-        service, password = data.split(":")
-        print("Service: " + service + " | Password: " + password)
+    add()
