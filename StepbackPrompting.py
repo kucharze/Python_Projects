137945c640ccc7eb97565f61fa60c988311ad75d
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2025-05-29 14:36:28 -0400

    New entry in examples list

--- StepbackPrompting.py
+++ StepbackPrompting.py
@@ -4,12 +4,17 @@ from langchain.utilities import DuckDuckGoSearchAPIWrapper
 import os
 from langchain.prompts import ChatPromptTemplate
 
+# Set your OpenAI API key here
+os.environ["OPENAI_API_KEY"] = "sk-xxxxxxxxxxxxxxx"
+
 examples = [
     {
         "input": "Can members of the police legally make arrests?",
         "output": "What can members of the police do?"
-    }
+    },
+    {
+        "input": "In what country was Jan Schinde born?",
+        "output": "What is Jan Schindel's background?"
+    },
 ]
 
-# Set your OpenAI API key here
-os.environ["OPENAI_API_KEY"] = "sk-xxxxxxxxxxxxxxx"
