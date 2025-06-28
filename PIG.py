f5537c99d93f254783a16f57a59b0cc6041870ef
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2025-06-28 13:56:14 -0400

    Removed unnecessary imports

--- PIG.py
+++ PIG.py
@@ -1,11 +1,6 @@
 # Code based on youtube video https://www.youtube.com/watch?v=NpmFbWO6HPU&t=19s
 
 import random
-from gtts import gTTS
-import os
-import time
-import sys
-import pyautogui
 
 def roll():
     min_val = 1
