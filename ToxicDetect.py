0663c42504a7acf48601997e1f76547688826d17
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2025-06-29 18:52:25 -0400

    Initial logic

--- ToxicDetect.py
+++ ToxicDetect.py
@@ -1,1 +1,8 @@
 bad_words = ['hate', 'dumb', 'stupid']
+
+sentence = input("Enter a sentence: ").lower()
+
+for word in bad_words:
+    if word in sentence:
+        print("Bad word detected: " + word)
+        break
