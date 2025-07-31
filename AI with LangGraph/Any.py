23851b072c048cca230d857aa2f71f1dfedfd22b
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2025-07-31 12:19:23 -0400

    Any example

---
+++ AI with LangGraph/Any.py
@@ -0,0 +1,9 @@
+from typing import Any
+
+def print_any(any_value: Any) -> None:
+    print(any_value)
+
+print_any(1)
+print_any("Hello")
+print_any([1, 2, 3])
+print_any({"name": "John", "age": 30})
