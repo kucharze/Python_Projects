67b996150d5b5d815473af8cf5af83177fadb9bb
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2026-07-22 11:45:29 -0400

    Added comment

--- Python Practice/first.py
+++ Python Practice/first.py
@@ -11,4 +11,5 @@ print("Hello World,\t I am learning Python programming")#Prints Hello World and 
 
 # Comments are used to explain code and make it more readable. Comments are ignored by the Python interpreter. 
 # You can use the # symbol to create a single-line comment, or triple quotes (""" """) to create a multi-line comment.
+#The below print func prints Hellow world
 print("Hello World") # This is a single-line comment
