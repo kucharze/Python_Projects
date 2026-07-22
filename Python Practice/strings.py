71753e4822bafa7c81e752c9e105a65201df08e3
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2026-07-22 13:27:36 -0400

    Set up string file

---
+++ Python Practice/strings.py
@@ -0,0 +1,5 @@
+# This file will be about strings and methods to manipulate them.
+# Strings are a sequence of characters. They are used to represent text. Strings are enclosed in quotes. You can use single or double quotes.
+
+name = "Root Admin User"
+print(len(name)) # Prints the length of the string. The len() function returns the number of characters in a string.
