0cd8be1b16d4175a033144938ec4c443d6d5279b
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2026-07-22 11:23:01 -0400

    Added comments

--- Python Practice/Beginningprogram.py
+++ Python Practice/Beginningprogram.py
@@ -1,4 +1,4 @@
 print("Hello World")
 print("I am learning Python programming")
-print(12+8)
-print("12+8")
+print(12+8)#Prints a number
+print("12+8")#Prints a string. Strings are always enclosed in quotes. You can use single or double quotes.
