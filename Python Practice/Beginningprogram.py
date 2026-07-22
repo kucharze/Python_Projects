48c834d5b2bb719d69cf39136d4a825c2120451c
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2026-07-22 11:33:50 -0400

    New code for escape sequnces

--- Python Practice/Beginningprogram.py
+++ Python Practice/Beginningprogram.py
@@ -4,3 +4,6 @@ print(12+8)#Prints a number
 print("12+8")#Prints a string. Strings are always enclosed in quotes. You can use single or double quotes.
 
 #Python can't split strings across multiple lines. You can use the backslash character to split a string across multiple lines. """ """
+
+#Escape sequences are used to represent special characters in strings. For example, \n represents a new line, and \t represents a tab.
+print("Hello World,\n I am learning Python programming")#Prints Hello World and I am learning Python programming on separate lines
