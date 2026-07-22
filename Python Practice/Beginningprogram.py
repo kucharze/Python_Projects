1e1f53c7e784711508d2e47ebb8cd425e4e0ed89
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2026-07-22 11:31:25 -0400

    Added comments

--- Python Practice/Beginningprogram.py
+++ Python Practice/Beginningprogram.py
@@ -2,3 +2,5 @@ print("Hello World")
 print("I am learning Python programming")
 print(12+8)#Prints a number
 print("12+8")#Prints a string. Strings are always enclosed in quotes. You can use single or double quotes.
+
+#Python can't split strings across multiple lines. You can use the backslash character to split a string across multiple lines. """ """
