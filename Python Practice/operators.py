ccbc7a4b44b5fbf8de231d65b143d95371e9c443
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2026-07-23 13:36:11 -0400

    Added comment notes for each arithmetic operation

--- Python Practice/operators.py
+++ Python Practice/operators.py
@@ -4,6 +4,14 @@
 
 #Arithmetic Operators
 # +, -, *, /, %, //, **
+# Used to perform basic mathematical operations on numbers. 
+# The + operator is used for addition, 
+# the - operator is used for subtraction, 
+# the * operator is used for multiplication, 
+# the / operator is used for division, 
+# the % operator is used for modulus (remainder), 
+# the // operator is used for floor division (returns the largest integer less than or equal to the result), 
+# and the ** operator is used for exponentiation (raises a number to a power).
 
 #Assignment Operators
 # =, +=, -=, *=, /=, %=, //=, **=, &=, |=, ^=, >>=, <<=
