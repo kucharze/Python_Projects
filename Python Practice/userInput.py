6c279ba59a029532f6d91b54bc81e67e1c8a998a
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2026-07-23 13:54:08 -0400

    Set up user Input file

---
+++ Python Practice/userInput.py
@@ -0,0 +1,4 @@
+#User input file
+
+# The input() function is used to take input from the user. It takes a string as an argument, which is displayed as a prompt to the user.
+name = input("Enter your name: ") # Takes input from the user and stores it in the variable name.
