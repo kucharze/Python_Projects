38ded0a0830c399fee0b4024466e5022b15ae014
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2026-07-23 13:53:16 -0400

    Added code logic to bool file

--- Python Practice/bool.py
+++ Python Practice/bool.py
@@ -2,3 +2,10 @@
 #Python has the following boolean data types:
 #True 1
 #False 0
+
+# Always used with logical operators.
+
+print(12>45) #Prints false
+print(12<45) #Prints true
+print(12==45) #Prints false
+print(12!=45) #Prints true
