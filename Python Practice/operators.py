1e78fe52a4ee7d907822c5eaa0c52cf372f7a3e7
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2026-07-23 13:32:15 -0400

    New comments to provide notes on types of operators

--- Python Practice/operators.py
+++ Python Practice/operators.py
@@ -1,1 +1,15 @@
 #Operators - symbols that are used to perform specific mathmatical and logical operations on variables and values.
+# Take one or more values as input and produce a single value as output. Operators are used to perform operations on variables and values. 
+# There are several types of operators in Python, including arithmetic, comparison, logical, assignment, and bitwise operators.
+
+#Arithmetic Operators
+# +, -, *, /, %, //, **
+
+#Assignment Operators
+# =, +=, -=, *=, /=, %=, //=, **=, &=, |=, ^=, >>=, <<=
+
+#Comparison Operators (Also know as Relational Operators)
+# ==, !=, >, <, >=, <=
+
+#Logical Operators
+# and, or, not
